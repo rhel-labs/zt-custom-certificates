1abfc069889e180a0b1b24447bc90333c1ef484e
Matt Micene (RHT) <mmicene@redhat.com>
2026-05-01 16:52:33 -0400

    Update solve script

--- runtime-automation/module-01/solve-rhel.sh
+++ runtime-automation/module-01/solve-rhel.sh
@@ -12,12 +12,9 @@ chown rhel:rhel /home/rhel/webserver/Containerfile
 runuser -l rhel << 'RHEL_EOF'
 podman build -t caddy:ssl -f ~/webserver/Containerfile ~/webserver
 podman run --rm -d --name caddy-ssl -p 8443:8443 -v ~/webserver:/usr/share/caddy:ro,Z caddy:ssl
-until podman exec caddy-ssl test -f /data/caddy/pki/authorities/local/root.crt 2>/dev/null; do sleep 1; done
-podman run --net=host --rm registry.access.redhat.com/hi/curl https://localhost:8443
+until podman cp caddy-ssl:/data/caddy/pki/authorities/local/root.crt ~/ 2>/dev/null; do sleep 1; done
 podman cp caddy-ssl:/data/caddy/pki/authorities/local/root.key ~/
-podman cp caddy-ssl:/data/caddy/pki/authorities/local/root.crt ~/
 cat ~/root.key ~/root.crt > ~/ca.pem
-ls -la ~/root.key ~/root.crt ~/ca.pem
 RHEL_EOF
 
 echo "Solve module-01 complete" >> /tmp/progress.log
